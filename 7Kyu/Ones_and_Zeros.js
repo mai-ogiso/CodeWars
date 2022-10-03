f0224709bff8479827e55d5d65cffdd87f6912fb
mai-ogiso <little.bellflower@gmail.com>
2022-10-04 03:39:40 +0900

    Ones_and_Zeros.js

---
+++ 7Kyu/Ones_and_Zeros.js
@@ -0,0 +1,8 @@
+// Ones and Zeros(7kyu)
+// Input [0, 0, 1, 0]
+// Output 2
+/* convert the equivalent binary value to an integer. */
+
+const binaryArrayToNumber = arr => {
+    return parseInt(arr.join(""),2)  
+  };
