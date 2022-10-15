4a131d40f05992d2c4ecc93cb6f1d0c9e47ce1bd
mai-ogiso <little.bellflower@gmail.com>
2022-10-16 04:40:28 +0900

    Coding_Meetup_2.js

---
+++ 7Kyu/Coding_Meetup_2.js
@@ -0,0 +1,29 @@
+// Coding Meetup #2 - Higher-Order Functions Series - Greet developers
+/*
+// Input
+var list1 = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+];
+// Output
+[
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+    greeting: 'Hi Sofia, what do you like the most about Java?'
+  },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+    greeting: 'Hi Lukas, what do you like the most about Python?'
+  },
+];
+*/
+var list = [
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+];
+
+function greetDevelopers(list) {
+  list.forEach(item => item.greeting = `Hi ${item.firstName}, what do you like the most about ${item.language}?`);
+  return list
+}
+
+console.log(greetDevelopers(list))
