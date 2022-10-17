dff8bd3238d21bd30180b3a18774e058b9be769b
mai-ogiso <little.bellflower@gmail.com>
2022-10-18 04:15:24 +0900

    Coding_Meetup_2.js

--- 7Kyu/Coding_Meetup_2.js
+++ 7Kyu/Coding_Meetup_2.js
@@ -18,7 +18,6 @@ var list1 = [
 var list = [
   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
-  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
 ];
 
 function greetDevelopers(list) {
