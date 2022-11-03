756ab40f4081a4d8188b6c1492f14f9c52fcb248
mai-ogiso <little.bellflower@gmail.com>
2022-11-04 05:04:16 +0900

    Number-Star_ladder.js

---
+++ 7Kyu/Number-Star_ladder.js
@@ -0,0 +1,16 @@
+// Number-Star ladder(7kyu)
+// Input pattern(3)
+/* Output "1\n1*2\n1**3"
+1
+1*2
+1**3
+*/
+
+function pattern1(n) {
+    return [...Array(n)]
+        .map((_, index) => index + 1)
+        .map(item => item === 1 ? 1 : "1" + "*".repeat(item - 1) + item)
+        .join("\n");
+}
+
+console.log(pattern(3))
