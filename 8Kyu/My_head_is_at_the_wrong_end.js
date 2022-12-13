71b935a69fbf6f96c006b4815a3131a01298f957
mai-ogiso <little.bellflower@gmail.com>
2022-12-14 04:10:38 +0900

    My_head_is_at_the_wrong_end.js

---
+++ 8Kyu/My_head_is_at_the_wrong_end.js
@@ -0,0 +1,7 @@
+// My head is at the wrong end!(8kyu)
+// Input [tail, body, head]
+// Output [head, body, tail]
+
+const fixTheMeerkat = arr => arr.reverse()
+
+console.log(fixTheMeerkat(["tail", "body", "head"]))
