a69aafed1f78de5dccf6008b893fb572c462b531
mai-ogiso <little.bellflower@gmail.com>
2022-12-30 15:16:04 +0900

    Sort_and_Star.js

---
+++ 8Kyu/Sort_and_Star.js
@@ -0,0 +1,13 @@
+// Sort and Star(8kyu)
+/*You will be given a list of strings. You must sort it alphabetically (case-sensitive, and based on the ASCII values of the chars) and then return the first value.
+The returned value must be a string, and have "***" between each of its letters.
+You should not remove or add elements from/to the array.
+*/
+
+function twoSort(s) {
+    const arr = s.slice()
+    const firstStr = arr.sort()[0]
+    return firstStr.split("").join("***")
+}
+
+console.log(twoSort(["bitcoin", "take", "over", "the", "world", "maybe", "who", "knows", "perhaps"]))
